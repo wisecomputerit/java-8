96aa55a3e25e6c10eb5f14ee471b85d53472758f
Wise Courses Ltd. <wise.computer.it@gmail.com>
2024-10-13 13:49:08 +0800

    Add files via upload

---
+++ Ch02/CodeWithComment.java
@@ -0,0 +1,11 @@
+// 以下就是我們所要撰寫的第一個Java程式
+public class CodeWithComment {
+	public static void main(String[] argv) {
+		// 到上面這兩行為止都是固定的程式骨架
+
+		// 在 main() 方法中就是我們要執行的程式
+		System.out.println("我的第一個Java程式。");
+
+		// 以下也都是固定的程式骨架
+	} // main() 方法的結束括號
+}   // CodeWithComment 的結束括號
---
+++ Ch02/FirstJava.java
@@ -0,0 +1,5 @@
+public class FirstJava {
+	public static void main(String[] argv) {
+		System.out.println("我的第一個Java程式。");
+	}
+}
